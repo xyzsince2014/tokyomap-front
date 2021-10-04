d4755f81871fd51b69a11853f7913109bbb2d454
xyzsince2014 <xyzsince2014@gmail.com>
2021-10-04 20:21:39 +0900

    refactored CustomMarkerContainer

--- src/containers/LeafletMap/CustomMarker.tsx
+++ src/containers/LeafletMap/CustomMarker.tsx
@@ -1,15 +1,12 @@
-import * as React from 'react';
-
 import useTimer from '../../hooks/LeafletMap/use-timer';
 import CustormMarker from '../../components/LeafletMap/CustomMarker';
 import {Tweet} from '../../services/socket/models';
 
-// todo: rename to EnhancedCustomMarkerProps
-export interface CustomMarkerProps {
+interface EnhancedCustomMarkerProps {
   tweet: Tweet;
 }
 
-const CustormMarkerContainer: React.FC<CustomMarkerProps> = ({tweet}) => {
+const CustormMarkerContainer: React.FC<EnhancedCustomMarkerProps> = ({tweet}) => {
   const timeRemaining = useTimer(tweet.disappearAt);
   return timeRemaining > 0 ? <CustormMarker tweet={tweet} timeRemaining={timeRemaining} /> : <></>;
 };
