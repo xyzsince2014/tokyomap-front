dc6a57321f6d7db3b60cd1fde4d70f20a9657d9c
xyzsince2014 <xyzsince2014@gmail.com>
2021-10-06 01:06:41 +0900

    refactored App & Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import * as React from 'react';
+import {useEffect} from 'react';
+import {Route, Switch, Redirect} from 'react-router';
+import {useHistory, useLocation, BrowserRouter} from 'react-router-dom';
 
-import Router from './router/Router';
+import LeafletMap from './containers/LeafletMap/LeafletMap';
 
-const App: React.FC = () => <Router />;
+const App: React.FC = () => {
+  const {action} = useHistory();
+  const {hash, pathname} = useLocation();
 
+  useEffect(() => {
+    if (!hash || action !== 'POP') {
+      window.scrollTo(0, 0);
+    }
+  }, [action, hash, pathname]);
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <LeafletMap />
+        </Route>
+        {/* todo: <Route path="/user/:userId"><User /></Route> */}
+        {/* todo: <Redirect from="/user/profile/:userId" to="/user/:userId" /> */}
+        <Redirect push to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 export default App;
