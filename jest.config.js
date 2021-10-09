3b0a3d3ddbb2d6251b6f27690750dcad7fae9f5a
xyzsince2014 <xyzsince2014@gmail.com>
2021-10-09 15:16:12 +0900

    upgraded & fixed jest

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.json',
     },
   },
   testMatch: ['<rootDir>/src/tests/**/*.spec.(ts|tsx)'],
