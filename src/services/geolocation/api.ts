ee093539cbefb1dc84f1239b4446fe15be9795dc
xyzsince2014 <xyzsince2014@gmail.com>
2021-10-10 12:26:35 +0900

    amended getGeolocation()

--- src/services/geolocation/api.ts
+++ src/services/geolocation/api.ts
@@ -1,9 +1,9 @@
 export const getGeolocationFactory = () => {
-  const getGeolocation = (): Promise<L.LatLngTuple> | void =>
+  const getGeolocation = (): Promise<L.LatLngTuple> =>
     new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(
         (pos: GeolocationPosition) => resolve([pos.coords.latitude, pos.coords.longitude]),
-        (err: GeolocationPositionError) => reject(new Error(`${err.message}`)),
+        (err: GeolocationPositionError) => reject(new Error('failed to get geolocation')),
         {
           enableHighAccuracy: true,
           timeout: 1000 * 10,
