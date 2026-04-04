778a3d4dd93696e8af1ef55ae780a884880d8340
xyzsince2014 <xyzsince2014@gmail.com>
2026-04-04 18:28:32 +0900

    fixed useTimer

--- src/hooks/leafletMap/useTimer.ts
+++ src/hooks/leafletMap/useTimer.ts
@@ -1,13 +1,12 @@
 import {useState, useEffect, useCallback, useRef} from 'react';
 
-import {fetchCurrentJst, formatDateTime} from '../../utils/dateTime';
-
 /** @param {string} disappearAt `yyyy-mm-ddThh:mm:ss.sssZ` */
 const useTimer = (disappearAt: string) => {
   /* the callback to calculate the tweet's remaining time to display */
   const getTimeRemaining = useCallback(
     () => {
-      const remaining = Math.floor((new Date(disappearAt).getTime() - fetchCurrentJst().getTime()) / 1000);
+      const jstOffsetMs = 9 * 60 * 60 * 1000;
+      const remaining = Math.floor((new Date(disappearAt).getTime() - jstOffsetMs - Date.now()) / 1000);
       return remaining;
     },
     [disappearAt],
